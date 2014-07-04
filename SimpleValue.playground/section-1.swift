d93b9aac95c810169906946a61ec5f1757072bbe
Fantasy Shao <forever.fantasy27@gmail.com>
2014-07-04 14:50:18 +0800

    Add simple value.

---
+++ SimpleValue.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Experiment in The Swift Programming Language
+//
+// Simple Value
+
+// Create a constant with an explict type of Float and a value of 4
+let constantDouble: Float = 4
+
+// Use \() to include a floating-point calculation in a string and to include someone's name in a greeting.
+let floatNum1: Float = 1.0
+let floatNum2: Float = 2.0
+let name: String = "Jack"
+let addString = "Hi, \(name). The float sum is \(floatNum1 + floatNum2)."
