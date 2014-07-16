c1de241389cb44bde760da1e326379c1d83c43d1
Fantasy Shao <forever.fantasy27@gmail.com>
2014-07-17 00:20:26 +0800

    Update Enumerations and Structures.

--- Enumerations&&Structures.playground/section-1.swift
+++ Enumerations&&Structures.playground/section-1.swift
@@ -2,7 +2,7 @@
 //
 // Enumerations and Structures
 
-// Write a function that compares two Rank values by comparing their raw values.
+// 1. Write a function that compares two Rank values by comparing their raw values.
 enum Rank: Int {
     case Ace = 1
     case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
@@ -32,7 +32,7 @@ let aceRawValue = ace.toRaw()
 // Try compare
 compare(Rank.Ace, Rank.Queen)
 
-// Add a color method to Suit that returns “black” for spades and clubs, and returns “red” for hearts and diamonds.
+// 2. Add a color method to Suit that returns “black” for spades and clubs, and returns “red” for hearts and diamonds.
 enum Suit {
     case Spades, Hearts, Diamonds, Clubs
     func simpleDescription() -> String {
@@ -60,4 +60,54 @@ enum Suit {
 let hearts = Suit.Hearts
 // Show color
 hearts.color()
+
+// 3. Add a method to Card that creates a full deck of cards, with one card of each combination of rank and suit.
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+    func getFullCard() -> Card[] {
+        var cards: Card[] = []
+        var suits: Suit[] = [.Spades, .Hearts, .Diamonds, .Clubs]
+        
+        for s in suits {
+            for i in 1...13 {
+                cards += Card(rank: Rank.fromRaw(i)!, suit: s)
+            }
+        }
+        
+        return cards
+    }
+}
+
+let threeOfSpades = Card(rank: .Three, suit: .Spades)
+threeOfSpades.simpleDescription()
+threeOfSpades.getFullCard()
+
+// 4. Add a third case to the ServerResponse and to the switch
+enum ServerResponse {
+    case Result(String, String)
+    case Error(String)
+    case Timeout(String)
+}
+
+let success = ServerResponse.Result("6:00 am", "8:09 pm")
+let failure = ServerResponse.Error("Out of cheese")
+let timeout = ServerResponse.Timeout("Time is out")
+
+switch success {
+case let .Result(sunrise, sunset):
+    let serverResponse = "Sunrise is at \(sunrise) and sunset is at \(sunset)."
+case let .Error(error):
+    let serverResponse = "Failuer.. \(error)"
+case let .Timeout(msg):
+    let serverResponse = "Server.. \(msg)"
+}
+
+
+
+
+
 
